37d9bf4bb082f7df2252ff996dfdf0b2ddbb72f8
Daniel Ivanov <d.ivanov1994@mail.bg>
2021-09-04 16:36:05 +0000

    Lab and exercise separate folders created.

--- Basic Syntax, Conditional Statements and Loops/multiplicationTable.js
+++
@@ -1,6 +0,0 @@
-function multiply(num) {
-    for (let i = 1; i <= 10; i++) {
-        let result = i * num;
-        console.log(`${num} X ${i} = ${result}`);
-    }
-}
--- Basic Syntax, Conditional Statements and Loops/rounding.js
+++
@@ -1,9 +0,0 @@
-function rounding(num, precision) {
-    if (precision > 15) {
-        precision = 15;
-    }
-    const roundedNum = num.toFixed(precision);
-    const withoutZeros = parseFloat(roundedNum);
-    console.log(withoutZeros);
-
-}
